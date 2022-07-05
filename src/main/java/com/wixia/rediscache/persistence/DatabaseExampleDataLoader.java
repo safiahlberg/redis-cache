e32094e3d2ab77e464bdf682f74f4cb0240a8460
Safi Ahlberg <safiahlberg@gmail.com>
2022-07-05 18:34:57 +0200

    Added relationship to the data

--- src/main/java/com/wixia/rediscache/persistence/DatabaseExampleDataLoader.java
+++ src/main/java/com/wixia/rediscache/persistence/DatabaseExampleDataLoader.java
@@ -10,15 +10,15 @@ public class DatabaseExampleDataLoader {
     @Bean
     CommandLineRunner init(CustomerRepository customerRepository, ItemRepository itemRepository) {
         return args -> {
-            customerRepository.save(new CustomerEo("Frodo", "Baggins"));
-            customerRepository.save(new CustomerEo("Bilbo", "Baggins"));
-            customerRepository.save(new CustomerEo("Samwise", "Gamgee"));
+            CustomerEo frodo = customerRepository.save(new CustomerEo("Frodo", "Baggins"));
+            CustomerEo bilbo = customerRepository.save(new CustomerEo("Bilbo", "Baggins"));
+            CustomerEo samwise = customerRepository.save(new CustomerEo("Samwise", "Gamgee"));
             customerRepository.save(new CustomerEo("Peregrin", "Took"));
             customerRepository.save(new CustomerEo("Meriadoc", "Brandybuck"));
 
-            itemRepository.save(new ItemEo("Lembas", 1));
-            itemRepository.save(new ItemEo("Mithril Shirt", 10000000));
-            itemRepository.save(new ItemEo("The One Ring", 1000000000));
+            itemRepository.save(new ItemEo("Lembas", 1, samwise));
+            itemRepository.save(new ItemEo("Mithril Shirt", 10000000, frodo));
+            itemRepository.save(new ItemEo("The One Ring", 1000000000, bilbo));
         };
     }
 }
