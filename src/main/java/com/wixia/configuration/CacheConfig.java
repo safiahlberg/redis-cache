e13cbe002216ff11f7d15e18540db320ea40ca99
Safi Ahlberg <safiahlberg@users.noreply.github.com>
2022-09-29 13:12:02 +0200

    Moved EnableCaching annotation. Still OK

--- src/main/java/com/wixia/configuration/CacheConfig.java
+++ src/main/java/com/wixia/configuration/CacheConfig.java
@@ -3,6 +3,7 @@ package com.wixia.configuration;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -18,6 +19,7 @@ import java.time.Duration;
  * https://docs.spring.io/spring-boot/docs/current/reference/html/application-properties.html#appendix.application-properties.cache
  */
 @Configuration
+@EnableCaching
 public class CacheConfig extends CachingConfigurerSupport implements CachingConfigurer {
 /*
     @Value("${redis.hostname:localhost}")
